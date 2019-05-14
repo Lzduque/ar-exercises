b720fffdf802c7667828374f4c9144e029c67633
Lzduque <lzduque@hotmail.com>
2019-05-14 17:52:49 +0000

    validations for inputs

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -11,10 +11,10 @@ puts "----------"
 # Your code goes here ...
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 @store1.employees.create(first_name: "Lisa", last_name: "Simpson", hourly_rate: 110)
-@store1.employees.create(first_name: "Bart", last_name: "Simpson", hourly_rate: 30)
+@store1.employees.create(first_name: "Bart", last_name: "Simpson", hourly_rate: 50)
 @store1.employees.create(first_name: "Rudolph", last_name: "Christmas", hourly_rate: 60)
 
 @store2.employees.create(first_name: "John", last_name: "Snow", hourly_rate: 90)
 @store2.employees.create(first_name: "Dany", last_name: "Stormborn", hourly_rate: 150)
 @store2.employees.create(first_name: "Cersei", last_name: "Lannister", hourly_rate: 110)
-@store2.employees.create(first_name: "Jack", last_name: "All-Trades", hourly_rate: 40)
+@store2.employees.create(first_name: "Jack", last_name: "All-Trades", hourly_rate: 45)
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
   has_many :employees
-end
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true , greater_than: 0 }
+  # validate :must_have_women_or_men_apparel
+  # validates :womens_apparel, presence: true
+  # validates :mens_apparel, presence: true
+
+  # def must_have_women_or_men_apparel
+  #   if womens_apparel.class == 'TrueClass' || mens_apparel.class == 'TrueClass'
+  #     errors.add(:womens_apparel, "must sell at least one type of apparel")
+  #   end
+  # end
+ end
