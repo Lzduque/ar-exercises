c80c927abbb5d300f206716d11f2e3aa2c7a3430
Lzduque <lzduque@hotmail.com>
2019-05-14 16:35:13 +0000

    exercise 4 - search stores that carrie men apparel

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -35,3 +35,10 @@ pp @store3
 @store3.destroy
 
 # -----------------EXERCISE 4-----------------
+
+surrey_store = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+whistler_store = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+yaletown_store = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+pp @mens_stores
