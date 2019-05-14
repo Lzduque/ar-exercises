c3a9c9659ce1ca37fb2e52079c66e01ef2ebad2e
Lzduque <lzduque@hotmail.com>
2019-05-14 17:24:41 +0000

    exercise 6 - create employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -13,3 +13,8 @@ puts "----------"
 @store1.employees.create(first_name: "Lisa", last_name: "Simpson", hourly_rate: 110)
 @store1.employees.create(first_name: "Bart", last_name: "Simpson", hourly_rate: 30)
 @store1.employees.create(first_name: "Rudolph", last_name: "Christmas", hourly_rate: 60)
+
+@store2.employees.create(first_name: "John", last_name: "Snow", hourly_rate: 90)
+@store2.employees.create(first_name: "Dany", last_name: "Stormborn", hourly_rate: 150)
+@store2.employees.create(first_name: "Cersei", last_name: "Lannister", hourly_rate: 110)
+@store2.employees.create(first_name: "Jack", last_name: "All-Trades", hourly_rate: 40)
